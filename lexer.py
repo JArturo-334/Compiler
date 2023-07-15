6393b75666d6b3bbd48e447a917d1097ffc54891
JArturo-334 <swdev.arturo.cb@gmail.com>
2023-07-15 14:28:32 -0600

    Lexer writes in lexical_reult.txt with syntax words

--- lexer.py
+++ lexer.py
@@ -6,6 +6,11 @@ accepting_states = transitions.accepting_states
 reserved_words = transitions.reserved_words
 
 
+# Clear content in lexical_result.txt
+with open('lexical_result.txt', 'w') as f:
+    f.write('')
+
+
 def check_word(word):
     # Initialize the automaton
     current_state = 'q0'
@@ -44,7 +49,7 @@ def check_word(word):
     }
 
     if word in reserved_words:
-        word_type = 'Reserved word'
+        word_type = word
 
     else:
         # Accumulate characters until no valid transition exists
@@ -66,14 +71,17 @@ def check_word(word):
         if current_state in accepting_states:
             word_type = accepting_states[current_state]
 
+    if word_type == 'operator':
+        word_type = word
+
     return word_type
 
 
 def check_symbol(sym):
-    if 'A' <= sym <= 'Z' or 'a' <= sym <= 'z':
+    if sym.isalpha():
         sym = 'letter'
 
-    if '0' <= sym <= '9':
+    if sym.isdigit():
         sym = 'digit'
 
     return sym
@@ -86,37 +94,69 @@ with open('code.txt', 'r') as file:
 word = ''
 current_state = 'q0'
 
+# Write in a new file
+
+
+def lexer_write(line):
+    with open('lexical_result.txt', 'a') as f:
+        f.write(f'{line} ')
+
+
+first_symbol = content[0]
+end_symbol = content[len(content)-1]
+
 for i, char in enumerate(content):
 
-    word += char
-
-    char = check_symbol(char)
-
-    next_char = content[i + 1] if i + 1 < len(content) else ''
-    next_char = check_symbol(next_char)
-
-    if i == len(content) - 1:
-        next_char = ''
-
-    current_state = transition_table[current_state].get(char, None)
-
-    if current_state == None:
-        if char == '\n':
-            word = ''
+    if i == len(content)-1:
+        if end_symbol == '.':
+            lexer_write(end_symbol)
+            continue
         else:
-            word_type = check_word(word)
+            print('Program must end with "." symbol')
+            break
+
+    if i == 0:
+        if first_symbol == '$':
+            lexer_write(first_symbol)
+            continue
+        else:
+            print('Program must start with $ symbol')
+            break
+
+    else:
+        word += char
+
+        char = check_symbol(char)
+
+        next_char = content[i + 1] if i + 1 < len(content) else ''
+        next_char = check_symbol(next_char)
+
+        if i == len(content) - 1:
+            next_char = ''
+
+        current_state = transition_table[current_state].get(char, None)
+
+        if current_state == None:
+            if char == '\n':
+                word = ''
+            else:
+                word_type = check_word(word.strip())
+                if word_type:
+                    lexer_write(word_type)
+
+                    print(f'{word.strip()} is a valid {word_type}')
+                    word = ''  # Reset the word to start accumulating the next word
+                    current_state = 'q0'
+
+            current_state = 'q0'
+
+        if current_state in accepting_states and next_char not in transition_table[current_state]:
+            # Exclude the current character
+            word_type = check_word(word.strip())
             if word_type:
-                print(f'{word.strip()} is a valid {word_type}')
+                lexer_write(word_type)
+                print(f'{word_type}')
                 word = ''  # Reset the word to start accumulating the next word
                 current_state = 'q0'
-
-        current_state = 'q0'
-
-    if current_state in accepting_states and next_char not in transition_table[current_state]:
-        word_type = check_word(word)  # Exclude the current character
-        if word_type:
-            print(f'{word.strip()} is a valid {word_type}')
-            word = ''  # Reset the word to start accumulating the next word
-            current_state = 'q0'
-        else:
-            print(f'{word} is invalid')
+            else:
+                print(f'{word} is invalid')
